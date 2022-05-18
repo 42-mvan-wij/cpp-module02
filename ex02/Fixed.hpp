53722c0c2f0fc9dd624eb1a293e6bcc87dab2fc0
Mariusmivw <mvan-wij@student.codam.nl>
2022-05-18 15:36:04 +0200

    add ex02

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,50 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed {
+	public:
+		Fixed();
+		Fixed(int const val);
+		Fixed(float const val);
+		Fixed(Fixed const &src);
+		Fixed &operator=(Fixed const &rhs);
+		~Fixed();
+
+		bool operator>(Fixed const &rhs) const;
+		bool operator<(Fixed const &rhs) const;
+		bool operator>=(Fixed const &rhs) const;
+		bool operator<=(Fixed const &rhs) const;
+		bool operator==(Fixed const &rhs) const;
+		bool operator!=(Fixed const &rhs) const;
+
+		Fixed operator+(Fixed const &rhs);
+		Fixed operator-(Fixed const &rhs);
+		Fixed operator*(Fixed const &rhs);
+		Fixed operator/(Fixed const &rhs);
+
+		Fixed &operator++(void);
+		Fixed &operator--(void);
+		Fixed operator++(int);
+		Fixed operator--(int);
+
+		static Fixed &min(Fixed &a, Fixed &b);
+		static Fixed const &min(Fixed const &a, Fixed const &b);
+		static Fixed &max(Fixed &a, Fixed &b);
+		static Fixed const &max(Fixed const &a, Fixed const &b);
+
+		int getRawBits() const;
+		void setRawBits(int const raw);
+
+		float toFloat(void) const;
+		int toInt(void) const;
+
+	private:
+		int fixedPointValue;
+		static const int fractionalBits = 8;
+};
+
+std::ostream &operator<<(std::ostream &o, Fixed const &fixed );
+
+#endif
