dc74af369187afd0829f978794f3fe87f2d6b5e8
Mariusmivw <mvan-wij@student.codam.nl>
2022-05-18 15:44:47 +0200

    Assign x and y in copy constructor

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -8,16 +8,17 @@ Point::Point(float const x, float const y) : x(x), y(y) {
 	// std::cout << "hello" << std::endl;
 }
 
-Point::Point(const Point &src) {
+Point::Point(const Point &src) : x(src.x), y(src.y) {
 	// std::cout << "nouh" << std::endl;
-	*this = src;
+	// *this = src;
 }
 
 Point &Point::operator=(const Point &rhs) {
 	if (this == &rhs)
 		return *this;
-	(Fixed &)this->x = rhs.x;
-	(Fixed &)this->y = rhs.y;
+	std::cerr << "FU" << std::endl;
+	// (Fixed &)this->x = rhs.x;
+	// (Fixed &)this->y = rhs.y;
 	return *this;
 }
 
