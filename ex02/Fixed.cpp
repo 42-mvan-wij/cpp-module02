ed4588c81f79f940f6097060adf9234eaedba2dd
Mariusmivw <mvan-wij@student.codam.nl>
2022-10-19 12:18:59 +0200

    Disabled debug output

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -3,23 +3,23 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed(): fixedPointValue(0) {
-	std::cout << "Default constructor called" << std::endl;
+	// std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(int const val): fixedPointValue(val << fractionalBits) {
-	std::cout << "Int constructor called" << std::endl;
+	// std::cout << "Int constructor called" << std::endl;
 }
 
 Fixed::Fixed(float const val): fixedPointValue(roundf(val * (1 << fractionalBits))) {
-	std::cout << "Float constructor called" << std::endl;
+	// std::cout << "Float constructor called" << std::endl;
 }
 
 Fixed::Fixed(Fixed const &src): fixedPointValue(src.fixedPointValue) {
-	std::cout << "Copy constructor called" << std::endl;
+	// std::cout << "Copy constructor called" << std::endl;
 }
 
 Fixed &Fixed::operator=(Fixed const &rhs) {
-	std::cout << "Copy assignment operator called" << std::endl;
+	// std::cout << "Copy assignment operator called" << std::endl;
 	if (this == &rhs)
 		return *this;
 	this->fixedPointValue = rhs.fixedPointValue;
@@ -27,7 +27,7 @@ Fixed &Fixed::operator=(Fixed const &rhs) {
 }
 
 Fixed::~Fixed() {
-	std::cout << "Destructor called" << std::endl;
+	// std::cout << "Destructor called" << std::endl;
 }
 
 int Fixed::getRawBits() const {
